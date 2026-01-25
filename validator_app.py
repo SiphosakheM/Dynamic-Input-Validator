8db9c2c81973e74114f520316b811980b52a6179
simsieec025 <simsieec025@student.wethinkcode.co.za>
2026-01-25 20:48:20 +0200

    improving my code

--- validator_app.py
+++ validator_app.py
@@ -1,30 +1,28 @@
-user_prompt = input("Enter your data here: ")
-def validate_input(user_prompt,validators):
-    while True:
-        for validator in validators:
-            if not validator(user_prompt):
-                return False
-        return True
-        
-def is_numeric(n):
-    for char in n:
-        if not char.isdigit():
+def validate_input(user_input, validators):
+    for validator in validators:
+        if not validator(user_input):
             return False
     return True
 
-def is_in_range(n):
-    num = int(n)
-    if num in range(1,101):
-        return True
-    else:
+
+def is_numeric(value):
+    return value.isdigit()
+
+
+def is_in_range(value):
+    try:
+        num = int(value)
+        return 1 <= num <= 100
+    except ValueError:
         return False
-    
-def no_space(space):
-    for char in space:
-        if char == " ":
-            return False
-    return True
-        
-result = [is_numeric, is_in_range,no_space]
-user_data = validate_input(user_prompt,result)
-print(f"is the data valid: {user_data}")
+
+
+def no_space(value):
+    return " " not in value
+
+
+user_input = input("Enter your data here: ")
+validators = [is_numeric, is_in_range, no_space]
+
+is_valid = validate_input(user_input, validators)
+print(f"Is the data valid: {is_valid}")
